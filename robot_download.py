e146207ca9e3bb373f39de895d7979de5630b5a5
Gogy-nogy <usernamesgo@gmail.com>
2022-07-21 19:08:43 +0530

    chto-to pomenial

--- robot_download.py
+++ robot_download.py
@@ -3,25 +3,25 @@ import time
 import os
 import wget
 
+if not os.path.exists("reclebin"):
+    os.mkdir("reclebin") #создает пустую папку для сохранения файлов
 folder = os.path.abspath(os.path.join("list_url.txt"))
 path = os.path.abspath(os.path.join("reclebin"))
 file = open(folder, "r")
 urls = file.readlines()
 file.close()
-def download_staf(url, path): #сама функция даунлоада
+downloaded_files = ()
+def download_staf(path): #сама функция даунлоада
+
     for url in urls:
-        if url in path:
-            print("файл уже скачан")
-        else:
-            wget.download(url, path)
-            print(f"загрузка завершена {url}")
+        wget.download(url, path)
+        print(f"загрузка завершена {url}")
 
 
 url = "https://www.python.org/ftp/python/3.8.9/python-3.8.9-amd64.exe"
 
 # schedule.every(7).day.at("16:20").do(download_staf, url, path) #вызывает функцию загрузки раз в неделю
-schedule.every(1).second.do(download_staf, url, path)
-
+schedule.every(1).second.do(download_staf, path)
 while True:
     schedule.run_pending()
     time.sleep(1)
