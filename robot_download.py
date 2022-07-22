3cb914a9eadf17c8c941a481ac37f9ab544802bf
Gogy-nogy <usernamesgo@gmail.com>
2022-07-22 10:58:34 +0530

    все краше и краше

--- robot_download.py
+++ robot_download.py
@@ -15,15 +15,14 @@ def download_staf(folder, path):  # функция даунлоуда
                 with open(f"reclebin/{fileName}", "rb") as f:
                      bytesFile = f.read()  # читает файл в байтах
                      readable_hash = hashlib.sha256(bytesFile).hexdigest()  # получает хеш 256
-                     print(readable_hash)
                      if folders[2] == readable_hash:  # сравнивает хеш нового файла с историей
                          print("актуальный файл уже скачан")
-                         os.remove(f'reclebin/{fileName}')
-                         # удаляет загруженный файл, если он соответствует файлу в базе
+                         os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
                      else:
                          print(f"по ссылке {folders[1]} найден новый файл {wget.filename_from_url(folders[1])}")
-                         # сюда нужно дописать выгрузку на aws
-                         os.remove(f'reclebin/{fileName}')
+                         if input("загрузить новый файл на aws? y/n").lower() == "y":
+                             pass  # сюда нужно дописать выгрузку на aws
+                         os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
             except:  # действие в случае не удавшейся загрузки файла
                 print("ни каких шишек, пока не докуришь гашиш")
 
