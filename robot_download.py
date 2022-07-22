88df0e56a10eed26f8adbd88f8eefbcbe6969907
Gogy-nogy <usernamesgo@gmail.com>
2022-07-22 10:32:00 +0530

    все краше и краше

--- robot_download.py
+++ robot_download.py
@@ -10,19 +10,20 @@ def download_staf(folder, path):  # функция даунлоуда
             folders = line.split(", ")  # готовый список для каждой загрузки 0 - имя файла, 1 - ссылка, 2 - хеш256
             try:  # пробует загрузить новый файл
                 wget.download(folders[1], path)  # загрузка файла
-                print(f"загрузка завершена {folders[0]}")
-                with open(f"reclebin/{folders[0]}", "rb") as f:
+                print(f"загрузка завершена {folders[1]}")
+                fileName = wget.filename_from_url(folders[1])
+                with open(f"reclebin/{fileName}", "rb") as f:
                      bytesFile = f.read()  # читает файл в байтах
                      readable_hash = hashlib.sha256(bytesFile).hexdigest()  # получает хеш 256
+                     print(readable_hash)
                      if folders[2] == readable_hash:  # сравнивает хеш нового файла с историей
                          print("актуальный файл уже скачан")
-                         os.remove(f'reclebin/{folders[0]}')
+                         os.remove(f'reclebin/{fileName}')
                          # удаляет загруженный файл, если он соответствует файлу в базе
                      else:
                          print(f"по ссылке {folders[1]} найден новый файл {wget.filename_from_url(folders[1])}")
-                         folders[0] = wget.filename_from_url(folders[1])  # обновление имени загруженного файла
-                         folders[2] = readable_hash  # обновление хеша
-                         pass
+                         # сюда нужно дописать выгрузку на aws
+                         os.remove(f'reclebin/{fileName}')
             except:  # действие в случае не удавшейся загрузки файла
                 print("ни каких шишек, пока не докуришь гашиш")
 
