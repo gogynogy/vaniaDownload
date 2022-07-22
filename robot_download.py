00ec731807d0c48b8a47931219d78e6d3357b995
Gogy-nogy <usernamesgo@gmail.com>
2022-07-22 18:49:48 +0530

    поправлен txt соответственно весь скрипт под него

--- robot_download.py
+++ robot_download.py
@@ -1,30 +1,34 @@
 import hashlib
+import yaml #pip install pyyaml
 import os
 import wget
 
 
 def download_staf(folder, path):  # функция даунлоуда
-    with open(folder) as file:  # открывает файл и читает его списком
-        lines = [line.rstrip('\n') for line in file]
-        for line in lines:
-            folders = line.split(", ")  # готовый список для каждой загрузки 0 - имя файла, 1 - ссылка, 2 - хеш256
+    with open('list_url.txt') as f:
+        templates = yaml.safe_load(f)  # получает словарь списков из файла list_url.txt
+        for file in templates:  # идем по спискам в словаре
+            name = file["name"]  # получаем имя файла
+            link = file["link"]  # получаем ссылку файла
+            sha256summ = file["sha256summ"]  # получаем хеш256 файла
             try:  # пробует загрузить новый файл
-                wget.download(folders[1], path)  # загрузка файла
-                print(f"загрузка завершена {folders[1]}")
-                fileName = wget.filename_from_url(folders[1])
+                print(f"начинается загузка с {link}")
+                wget.download(link, path)  # загрузка файла
+                print(f"загрузка {name} завершена")
+                fileName = wget.filename_from_url(link)
                 with open(f"reclebin/{fileName}", "rb") as f:
-                     bytesFile = f.read()  # читает файл в байтах
-                     readable_hash = hashlib.sha256(bytesFile).hexdigest()  # получает хеш 256
-                     if folders[2] == readable_hash:  # сравнивает хеш нового файла с историей
-                         print("актуальный файл уже скачан")
-                         os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
-                     else:
-                         print(f"по ссылке {folders[1]} найден новый файл {wget.filename_from_url(folders[1])}")
-                         if input("загрузить новый файл на aws? y/n").lower() == "y":
-                             pass  # сюда нужно дописать выгрузку на aws
-                         os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
+                    bytesFile = f.read()  # читает файл в байтах
+                    readable_hash = hashlib.sha256(bytesFile).hexdigest()  # получает хеш 256
+                    if sha256summ == readable_hash:  # сравнивает хеш нового файла с историей
+                        print("актуальный файл уже скачан")
+                        os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
+                    else:
+                        print(f"по ссылке {link} найден новый файл {wget.filename_from_url(link)}")
+                        if input("загрузить новый файл на aws? y/n").lower() == "y":
+                            pass  # сюда нужно дописать выгрузку на aws
+                        os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
             except:  # действие в случае не удавшейся загрузки файла
-                print("ни каких шишек, пока не докуришь гашиш")
+                print(f"по ссылке: {link} файла не обнаружено")
 
 
 if not os.path.exists("reclebin"):
