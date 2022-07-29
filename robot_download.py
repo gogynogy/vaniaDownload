e70632944772a4c7e1da01005917eb06c4232a63
Gogy-nogy <usernamesgo@gmail.com>
2022-07-29 09:15:43 +0530

    согласно замечаниям

--- robot_download.py
+++ robot_download.py
@@ -5,35 +5,30 @@ import wget
 import threading
 
 
-def download_staf(file, path):  # функция даунлоуда
-    name, link, sha256summ = file["name"], file["link"], file["sha256summ"]  # получаем имя, ссылку хеш256 файла
-    try:  # пробует загрузить новый файл
-        print(f"начинается загузка с {link}")
-        wget.download(link, path)  # загрузка файла
-        print(f"загрузка {name} завершена")
+def download_staf(file, path):
+    name, link, sha256summ = file["name"], file["link"], file["sha256summ"]
+    try:
+        wget.download(link, path)
         fileName = wget.filename_from_url(link)
         with open(f"reclebin/{fileName}", "rb") as f:
-            bytesFile = f.read()  # читает файл в байтах
-            readable_hash = hashlib.sha256(bytesFile).hexdigest()  # получает хеш 256
-            if sha256summ == readable_hash:  # сравнивает хеш нового файла с историей
-                print("актуальный файл уже скачан")
+            bytesFile = f.read()
+            readable_hash = hashlib.sha256(bytesFile).hexdigest()
+            if sha256summ == readable_hash:
+                os.remove(f'reclebin/{fileName}')
             else:
-                print(f"по ссылке {link} найден новый файл {wget.filename_from_url(link)}")
-                if input("загрузить новый файл на aws? y/n ").lower() == "y":
-                    pass  # сюда нужно дописать выгрузку на aws
-            os.remove(f'reclebin/{fileName}')  # удаляет загруженный файл
-    except:  # действие в случае не удавшейся загрузки файла
-        print(f"по ссылке: {link} файла не обнаружено")
+                pass
+    except:
+        pass
 
 
-if not os.path.exists("reclebin"):
-    os.mkdir("reclebin")  # создает пустую папку для временного сохранения файлов
+if not os.path.exists("fresh soft"):
+    os.mkdir("fresh soft")
 
-folder = os.path.abspath(os.path.join("list_url.txt"))  # путь к базе
-path = os.path.abspath(os.path.join("reclebin"))
-# with ThreadPoolExecutor(max_workers=4) as executor:
+folder = os.path.abspath(os.path.join("list_url.txt"))
+path = os.path.abspath(os.path.join("fresh soft"))
+
 with open(folder) as f:
-    templates = yaml.safe_load(f)  # получает словарь списков из файла list_url.txt
-    for file in templates:  # идем по спискам в словаре
+    templates = yaml.safe_load(f)
+    for file in templates:
         my_thread = threading.Thread(target=download_staf, args=(file, path))
-        my_thread.start()  # запускаем потоками функцию загрузки
+        my_thread.start()
