b33ad7c345f4f86770c8313764fa109261cefffa
gogynogy <usernamesgo@gmail.com>
2022-08-30 00:35:59 +0530

    done

--- downloadBot2.py
+++ downloadBot2.py
@@ -1,12 +1,10 @@
-import hashlib, ssl, os, wget, threading, boto3, yaml #pip install pyyaml
+import hashlib, ssl, os, wget, threading, random, boto3, yaml #pip install pyyaml
 ssl._create_default_https_context = ssl._create_unverified_context
 
 import telebot
 bot = telebot.TeleBot("5645949742:AAGDmze2SXINA1kqc4UJjAsSVVNaE9aDjik")
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-	bot.reply_to(message, "грузим сиськи!")
+
 
 id_gosha = 498332094
 
@@ -18,21 +16,17 @@ siski = os.path.abspath(os.path.join("siski"))
 def download_staf(file, path):
     try:
         name, link, sha256summ = file["name"], file["link"], file["sha256summ"]
-        print('3')
         wget.download(link, path)
-        print('4')
         fileName = wget.filename_from_url(link)
         with open(f"{path}/{fileName}", "rb") as file:
             bytesFile = file.read()
             readable_hash = hashlib.sha256(bytesFile).hexdigest()
             if sha256summ != readable_hash:
-                print("33")
                 os.remove(f'{path}/{fileName}')
             else:
-                print('1')
-                gif = open('221928.gif', 'rb')
+                filename = random.choice(os.listdir(siski))
+                gif = open(f'{siski}/{filename}', 'rb')
                 bot.send_animation(id_gosha, gif)
-                print("2")
                 #
                 # if name not in s3.buckets.all:
                 #     s3.download_file(name)
@@ -50,8 +44,13 @@ def start():
         templates = yaml.safe_load(f)
         for file in templates:
             download_staf(file, path)
-            my_thread = threading.Thread(target=download_staf, args=(file, path))
-            my_thread.start()
+
+
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, "грузим сиськи!")
 
 
 start()
